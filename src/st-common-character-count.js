84a05ff3c498536d895b0e455fb9752a22736f0d
User0k <glucus2@yandex.ru>
2021-09-13 20:46:41 +0300

    faet: add st-common-character-count

--- src/st-common-character-count.js
+++ src/st-common-character-count.js
@@ -1,5 +1,3 @@
-import { NotImplementedError } from '../extensions/index.js';
-
 /**
  * Given two strings, find the number of common characters between them.
  *
@@ -11,7 +9,15 @@ import { NotImplementedError } from '../extensions/index.js';
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-export default function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ export default function getCommonCharacterCount(s1, s2) {
+  let res = 0;
+  for (let i = 0; i < s1.length; i++) {
+    const reg = new RegExp(s1[i]);
+    if (reg.test(s2)) {
+      res++;
+      s2 = s2.replace(reg, '');
+    }
+  }
+
+  return res;
 }
