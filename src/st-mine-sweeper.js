2e4207de2c94c3ef3bcc6c19925fc19216dabaca
User0k <glucus2@yandex.ru>
2021-09-18 20:22:55 +0300

    feat: add st-mine-sweeper

--- src/st-mine-sweeper.js
+++ src/st-mine-sweeper.js
@@ -1,5 +1,3 @@
-import { NotImplementedError } from '../extensions/index.js';
-
 /**
  * In the popular Minesweeper game you have a board with some mines and those cells
  * that don't contain a mine have a number in it that indicates the total number of mines
@@ -23,7 +21,45 @@ import { NotImplementedError } from '../extensions/index.js';
  *  [1, 1, 1]
  * ]
  */
-export default function minesweeper (/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function minesweeper (matrix) {
+  const DIRECTIONS = [
+    [-1, -1],
+    [-1, 0],
+    [-1, 1],
+    [1, -1],
+    [1, 0],
+    [1, 1],
+    [0, 1],
+    [0, -1],
+  ];
+
+  const mineMatrix  = [];
+  //fills the field with zeroes;
+  matrix.forEach(arr => mineMatrix.push(Array(arr.length).fill(0)));
+
+  //defines if we are still in the matrix;
+  function withinMatrix(x, y, arr) {
+    return x >= 0 && y >= 0 && x < arr.length && y < arr[0].length;
+  }
+
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[0].length; j++) {
+      DIRECTIONS.forEach(move => {
+        const dX = i + move[0];
+        const dY= j + move[1];
+        if (withinMatrix(dX, dY, matrix) && matrix[dX][dY]) {
+          mineMatrix[i][j]++;
+        };
+
+      })
+    }
+  }
+
+  return mineMatrix;
 }
+
+console.log(minesweeper([
+    [true, false, false],
+    [false, true, false],
+    [false, false, false]
+   ]))
