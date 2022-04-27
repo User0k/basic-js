fea1485a828cb8144d17ec3b308158e3dd3b34c7
User0k <glucus2@yandex.ru>
2022-04-27 22:17:33 +0300

    feat: implement the very beginning of Vigenere cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -3,30 +3,210 @@ const { NotImplementedError } = require('../extensions/index.js');
 /**
  * Implement class VigenereCipheringMachine that allows us to create
  * direct and reverse ciphering machines according to task description
- * 
+ *
  * @example
- * 
+ *
  * const directMachine = new VigenereCipheringMachine();
- * 
+ *
  * const reverseMachine = new VigenereCipheringMachine(false);
- * 
+ *
  * directMachine.encrypt('attack at dawn!', 'alphonse') => 'AEIHQX SX DLLU!'
- * 
+ *
  * directMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => 'ATTACK AT DAWN!'
- * 
+ *
  * reverseMachine.encrypt('attack at dawn!', 'alphonse') => '!ULLD XS XQHIEA'
- * 
+ *
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
- * 
+ *
  */
+ const TABULA_RECTA = [
+  [
+    'A', 'B', 'C', 'D', 'E', 'F',
+    'G', 'H', 'I', 'K', 'L', 'M',
+    'N', 'O', 'P', 'Q', 'R', 'S',
+    'T', 'U', 'V', 'W', 'X', 'Y',
+    'Z'
+  ],
+  [
+    'B', 'C', 'D', 'E', 'F', 'G',
+    'H', 'I', 'K', 'L', 'M', 'N',
+    'O', 'P', 'Q', 'R', 'S', 'T',
+    'U', 'V', 'W', 'X', 'Y', 'Z',
+    'A'
+  ],
+  [
+    'C', 'D', 'E', 'F', 'G', 'H',
+    'I', 'K', 'L', 'M', 'N', 'O',
+    'P', 'Q', 'R', 'S', 'T', 'U',
+    'V', 'W', 'X', 'Y', 'Z', 'A',
+    'B'
+  ],
+  [
+    'D', 'E', 'F', 'G', 'H', 'I',
+    'K', 'L', 'M', 'N', 'O', 'P',
+    'Q', 'R', 'S', 'T', 'U', 'V',
+    'W', 'X', 'Y', 'Z', 'A', 'B',
+    'C'
+  ],
+  [
+    'E', 'F', 'G', 'H', 'I', 'K',
+    'L', 'M', 'N', 'O', 'P', 'Q',
+    'R', 'S', 'T', 'U', 'V', 'W',
+    'X', 'Y', 'Z', 'A', 'B', 'C',
+    'D'
+  ],
+  [
+    'F', 'G', 'H', 'I', 'K', 'L',
+    'M', 'N', 'O', 'P', 'Q', 'R',
+    'S', 'T', 'U', 'V', 'W', 'X',
+    'Y', 'Z', 'A', 'B', 'C', 'D',
+    'E'
+  ],
+  [
+    'G', 'H', 'I', 'K', 'L', 'M',
+    'N', 'O', 'P', 'Q', 'R', 'S',
+    'T', 'U', 'V', 'W', 'X', 'Y',
+    'Z', 'A', 'B', 'C', 'D', 'E',
+    'F'
+  ],
+  [
+    'H', 'I', 'K', 'L', 'M', 'N',
+    'O', 'P', 'Q', 'R', 'S', 'T',
+    'U', 'V', 'W', 'X', 'Y', 'Z',
+    'A', 'B', 'C', 'D', 'E', 'F',
+    'G'
+  ],
+  [
+    'I', 'K', 'L', 'M', 'N', 'O',
+    'P', 'Q', 'R', 'S', 'T', 'U',
+    'V', 'W', 'X', 'Y', 'Z', 'A',
+    'B', 'C', 'D', 'E', 'F', 'G',
+    'H'
+  ],
+  [
+    'K', 'L', 'M', 'N', 'O', 'P',
+    'Q', 'R', 'S', 'T', 'U', 'V',
+    'W', 'X', 'Y', 'Z', 'A', 'B',
+    'C', 'D', 'E', 'F', 'G', 'H',
+    'I'
+  ],
+  [
+    'L', 'M', 'N', 'O', 'P', 'Q',
+    'R', 'S', 'T', 'U', 'V', 'W',
+    'X', 'Y', 'Z', 'A', 'B', 'C',
+    'D', 'E', 'F', 'G', 'H', 'I',
+    'K'
+  ],
+  [
+    'M', 'N', 'O', 'P', 'Q', 'R',
+    'S', 'T', 'U', 'V', 'W', 'X',
+    'Y', 'Z', 'A', 'B', 'C', 'D',
+    'E', 'F', 'G', 'H', 'I', 'K',
+    'L'
+  ],
+  [
+    'N', 'O', 'P', 'Q', 'R', 'S',
+    'T', 'U', 'V', 'W', 'X', 'Y',
+    'Z', 'A', 'B', 'C', 'D', 'E',
+    'F', 'G', 'H', 'I', 'K', 'L',
+    'M'
+  ],
+  [
+    'O', 'P', 'Q', 'R', 'S', 'T',
+    'U', 'V', 'W', 'X', 'Y', 'Z',
+    'A', 'B', 'C', 'D', 'E', 'F',
+    'G', 'H', 'I', 'K', 'L', 'M',
+    'N'
+  ],
+  [
+    'P', 'Q', 'R', 'S', 'T', 'U',
+    'V', 'W', 'X', 'Y', 'Z', 'A',
+    'B', 'C', 'D', 'E', 'F', 'G',
+    'H', 'I', 'K', 'L', 'M', 'N',
+    'O'
+  ],
+  [
+    'Q', 'R', 'S', 'T', 'U', 'V',
+    'W', 'X', 'Y', 'Z', 'A', 'B',
+    'C', 'D', 'E', 'F', 'G', 'H',
+    'I', 'K', 'L', 'M', 'N', 'O',
+    'P'
+  ],
+  [
+    'R', 'S', 'T', 'U', 'V', 'W',
+    'X', 'Y', 'Z', 'A', 'B', 'C',
+    'D', 'E', 'F', 'G', 'H', 'I',
+    'K', 'L', 'M', 'N', 'O', 'P',
+    'Q'
+  ],
+  [
+    'S', 'T', 'U', 'V', 'W', 'X',
+    'Y', 'Z', 'A', 'B', 'C', 'D',
+    'E', 'F', 'G', 'H', 'I', 'K',
+    'L', 'M', 'N', 'O', 'P', 'Q',
+    'R'
+  ],
+  [
+    'T', 'U', 'V', 'W', 'X', 'Y',
+    'Z', 'A', 'B', 'C', 'D', 'E',
+    'F', 'G', 'H', 'I', 'K', 'L',
+    'M', 'N', 'O', 'P', 'Q', 'R',
+    'S'
+  ],
+  [
+    'U', 'V', 'W', 'X', 'Y', 'Z',
+    'A', 'B', 'C', 'D', 'E', 'F',
+    'G', 'H', 'I', 'K', 'L', 'M',
+    'N', 'O', 'P', 'Q', 'R', 'S',
+    'T'
+  ],
+  [
+    'V', 'W', 'X', 'Y', 'Z', 'A',
+    'B', 'C', 'D', 'E', 'F', 'G',
+    'H', 'I', 'K', 'L', 'M', 'N',
+    'O', 'P', 'Q', 'R', 'S', 'T',
+    'U'
+  ],
+  [
+    'W', 'X', 'Y', 'Z', 'A', 'B',
+    'C', 'D', 'E', 'F', 'G', 'H',
+    'I', 'K', 'L', 'M', 'N', 'O',
+    'P', 'Q', 'R', 'S', 'T', 'U',
+    'V'
+  ],
+  [
+    'X', 'Y', 'Z', 'A', 'B', 'C',
+    'D', 'E', 'F', 'G', 'H', 'I',
+    'K', 'L', 'M', 'N', 'O', 'P',
+    'Q', 'R', 'S', 'T', 'U', 'V',
+    'W'
+  ],
+  [
+    'Y', 'Z', 'A', 'B', 'C', 'D',
+    'E', 'F', 'G', 'H', 'I', 'K',
+    'L', 'M', 'N', 'O', 'P', 'Q',
+    'R', 'S', 'T', 'U', 'V', 'W',
+    'X'
+  ],
+  [
+    'Z', 'A', 'B', 'C', 'D', 'E',
+    'F', 'G', 'H', 'I', 'K', 'L',
+    'M', 'N', 'O', 'P', 'Q', 'R',
+    'S', 'T', 'U', 'V', 'W', 'X',
+    'Y'
+  ]
+]
+
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor () {
+    this.direct = true;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  encrypt(message, key) {
+    if (message === undefined || key === undefined) throw new Error('Incorrect arguments!');
+
+  }
+  decrypt(message, key) {
+    if (message === undefined || key === undefined) throw new Error('Incorrect arguments!');
   }
 }
 
